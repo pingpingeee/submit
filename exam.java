5b34ca61dbe09c0495dccbd2a756ae4fe03fd4c0
manamjong <nskynet5374@naver.com>
2025-02-24 10:17:34 +0900

    2025.02.24 평가시험

--- exam.java
+++ exam.java
@@ -3,11 +3,12 @@ import java.util.Random;
 public class exam {
     public static void main(String[] args) {
         int[][] numArr = new int[3][4];
+        Random random = new Random();
         int sum = 0;
 
         for (int i = 0; i < numArr.length; i++) {
             for (int j = 0; j < numArr[0].length; j++) {
-                numArr[i][j] = new Random().nextInt(9) + 1;
+                numArr[i][j] = random.nextInt(9) + 1;
                 System.out.print(numArr[i][j] + "\t");
                 sum += numArr[i][j];
             }
